c7fb63f52d80afbfcd6441cdd2810570ef73871f
HazemUsama <hazemosama681@gmail.com>
2024-07-15 06:07:18 +0300

    Fix order

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,19 +3,24 @@
 const request = require('request');
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
-request(url, { json: true }, (err, res) => {
-  if (err) {
-    return console.error('Request failed:', err);
-  }
-
-  const characters = res.body.characters;
-  for (const characterUrl of characters) {
-    request(characterUrl, { json: true }, (err, res) => {
-      if (err) {
-        return console.error('Request failed:', err);
-      }
-      const character = res.body.name;
-      console.log(character);
-    });
+request(url, { json: true }, async (err, res) => {
+  if (!err) {
+    const characters = res.body.characters;
+    for (const characterUrl of characters) {
+      await printCharacter(characterUrl)
+    }
   }
 });
+
+async function printCharacter (characterUrl) {
+  const res = await new Promise((resolve, reject) => {
+    request(characterUrl, { json: true } ,(error, res) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(res.body.name);
+      }
+    });
+  });
+  console.log(res);
+}
