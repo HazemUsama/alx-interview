13595abfddc9f0ba80f4a0ff577c08042e71db67
HazemUsama <hazemosama681@gmail.com>
2024-07-16 09:24:01 +0300

    Fix style

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,14 +7,14 @@ request(url, { json: true }, async (err, res) => {
   if (!err) {
     const characters = res.body.characters;
     for (const characterUrl of characters) {
-      await printCharacter(characterUrl)
+      await printCharacter(characterUrl);
     }
   }
 });
 
 async function printCharacter (characterUrl) {
   const res = await new Promise((resolve, reject) => {
-    request(characterUrl, { json: true } ,(error, res) => {
+    request(characterUrl, { json: true }, (error, res) => {
       if (error) {
         reject(error);
       } else {
