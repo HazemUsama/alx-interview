68f673fa60e981f7afc0cc9d8f149668dd70807d
Hazem Osama <hazem@debian.168.1.20>
2024-10-03 22:11:10 +0300

    use map instead of list

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,14 +8,18 @@ def makeChange(coins, total):
     """ Returns the minimum number of coins needed
         to make the given total
     """
-    dp = [0] + [total + 1] * total
+    dp = { 0: 0 }
 
     for i in range(1, total + 1):
         for coin in coins:
             if i - coin < 0:
                 continue
-            dp[i] = min(dp[i - coin] + 1, dp[i])
+            if (i - coin) in dp:
+                if i not in dp:
+                    dp[i] = dp[i - coin] + 1
+                else:
+                    dp[i] = min(dp[i - coin] + 1, dp[i])
 
-    if dp[total] > total:
+    if total not in dp:
         return -1
     return dp[total]
